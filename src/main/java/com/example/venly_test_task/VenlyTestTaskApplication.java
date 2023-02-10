7e6637f0f92c81072474128dbf94bda11dcb44f3
YaTut1901 <ovanya1901@gmail.com>
2023-02-10 12:48:55 +0200

    init_commit

---
+++ src/main/java/com/example/venly_test_task/VenlyTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.venly_test_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenlyTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VenlyTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/venly_test_task/VenlyTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.venly_test_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenlyTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
